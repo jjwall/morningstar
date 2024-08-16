6ee0239ce033ab5ca427c606c7b2852df325afeb
Jacob <jacob.wallace@utexas.edu>
2024-08-15 22:14:40 -0500

    second button setup

---
+++ gui/scenes/main.gd
@@ -0,0 +1,23 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_button_1_pressed() -> void:
+	var outputs = []
+	OS.execute('../scripts/dist/morningstar_scripts/test_script1.exe', [], outputs)
+	print(outputs[0].split('\r\n'))
+
+
+func _on_button_2_pressed() -> void:
+	var outputs = []
+	OS.execute('../scripts/dist/morningstar_scripts/test_script2.exe', [], outputs)
+	print(outputs[0].split('\r\n'))
--- scripts/test_script1.py
+++ scripts/test_script1.py
@@ -1,8 +1,6 @@
 def main():
     result = "hello from test_script1"
     print(result) # will need to json.stringify
-    print(result)
-    print(result)
     return result
 
 if __name__ == "__main__":
