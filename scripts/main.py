27b57ca8362e19b82cff6b8cf10c5452d94453a6
Jacob <jacob.wallace@utexas.edu>
2024-08-16 22:41:01 -0500

    subparsing architecture set up. Initializing picosdk args

--- scripts/main.py
+++ scripts/main.py
@@ -1,19 +1,26 @@
+# Custom modules
+from modules.picosdk import picosdk_utils
+
+# Packages
 import argparse
 
-def main(arg1: int, arg2: str):
-    result = "hello from test_script1"
-    print(arg1 + 7)
-    print(arg2)
-    print(result) # will need to json.stringify
-    return result
+def main():
+    # result = "hello from test_script1"
+    # print(arg1 + 7)
+    # print(arg2)
+    # print(result) # will need to json.stringify
+    # return result
+    args = parse_args()
+    args.func(args)
 
-# TODO: Use subcommands instead of multiple entry points.
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("my_integer", type=int)
-    parser.add_argument("my_string")
+    subparsers = parser.add_subparsers(required=True)
+    picosdk_utils.init_subparsers(subparsers)
+
+    # parser.add_argument("my_integer", type=int)
+    # parser.add_argument("my_string")
     return parser.parse_args()
 
 if __name__ == "__main__":
-    args = parse_args()
-    main(args.my_integer, args.my_string)
+    main()
---
+++ scripts/modules/picosdk/picosdk_utils.py
@@ -0,0 +1,10 @@
+def init_subparsers(subparsers):
+    parser_test_picosdk_module = subparsers.add_parser('test_picosdk_module')
+    parser_test_picosdk_module.add_argument('my_integer', type=int)
+    parser_test_picosdk_module.add_argument('my_string')
+    parser_test_picosdk_module.set_defaults(func=test_picosdk_module)
+
+def test_picosdk_module(args):
+    print(args.my_integer)
+    print(args.my_string)
+    print("hi there picosdk")
