700cfff170a41d8e7d4ae877e13d26510dbc0ec4
Jacob <jacob.wallace@utexas.edu>
2024-08-16 23:04:19 -0500

    python module architecture changes

--- scripts/main.py
+++ scripts/main.py
@@ -1,22 +1,17 @@
 # Custom modules
-from modules.picosdk import picosdk_utils
-
+from modules import picosdk
 # Packages
 import argparse
 
+# TODO: test out json.stringify outputs
 def main():
-    # result = "hello from test_script1"
-    # print(arg1 + 7)
-    # print(arg2)
-    # print(result) # will need to json.stringify
-    # return result
     args = parse_args()
     args.func(args)
 
 def parse_args():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(required=True)
-    picosdk_utils.init_subparsers(subparsers)
+    picosdk.init_subparsers(subparsers)
 
     # parser.add_argument("my_integer", type=int)
     # parser.add_argument("my_string")
--- scripts/modules/picosdk/utils.py
+++ scripts/modules/picosdk/utils.py
@@ -1,10 +1,12 @@
-def init_subparsers(subparsers):
+import argparse
+
+def init_subparsers(subparsers: argparse._SubParsersAction):
     parser_test_picosdk_module = subparsers.add_parser('test_picosdk_module')
     parser_test_picosdk_module.add_argument('my_integer', type=int)
     parser_test_picosdk_module.add_argument('my_string')
-    parser_test_picosdk_module.set_defaults(func=test_picosdk_module)
+    parser_test_picosdk_module.set_defaults(func=_test_picosdk_module)
 
-def test_picosdk_module(args):
+def _test_picosdk_module(args):
     print(args.my_integer)
     print(args.my_string)
     print("hi there picosdk")
