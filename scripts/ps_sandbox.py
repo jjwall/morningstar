24781baa1cad3d24ea9984c7c6dc4fe1b2494c47
Jacob <jacob.wallace@utexas.edu>
2024-08-18 13:59:29 -0500

    opening and closing picoscope in sandbox script

--- scripts/ps_sandbox.py
+++ scripts/ps_sandbox.py
@@ -3,7 +3,33 @@
 # find_all_units()
 
 # find_unit()
+import ctypes
 
-from lib.picosdk.ps5000a import ps5000a
+from lib.picosdk.ps5000a import ps5000a as ps
+from lib.picosdk.functions import adc2mV, assert_pico_ok, mV2adc
 
-ps5000a.open_unit()
+chandle = ctypes.c_int16()
+# status = {}
+
+
+resolution = ps.PS5000A_DEVICE_RESOLUTION["PS5000A_DR_12BIT"]
+device = ps.open_unit(None, resolution)
+
+# status["openunit"] = ps.ps5000aOpenUnit(ctypes.byref(chandle), None, resolution)
+
+# try:
+#     assert_pico_ok(status["openunit"])
+# except: # PicoNotOkError:
+#     print("PicoNotOkError")
+#     powerStatus = status["openunit"]
+
+#     if powerStatus == 286:
+#         status["changePowerSource"] = ps.ps5000aChangePowerSource(chandle, powerStatus)
+#     elif powerStatus == 282:
+#         status["changePowerSource"] = ps.ps5000aChangePowerSource(chandle, powerStatus)
+#     else:
+#         raise
+
+#     assert_pico_ok(status["changePowerSource"])
+
+ps.close_unit(device)
