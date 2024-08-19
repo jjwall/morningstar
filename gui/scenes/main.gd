32e0dfd97fbd44fc6bd2dd613872ce5ca8eef703
Jacob <jacob.wallace@utexas.edu>
2024-08-18 23:56:27 -0500

    Added logger in GUI. Need to think about how I could do an autoload with this.

--- gui/scenes/main.gd
+++ gui/scenes/main.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$Console.text = ""
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,9 +17,13 @@ func _on_button_1_pressed() -> void:
 	#$WavelengthChart.plot_chart(voltages)
 	#print(voltages)
 	#$BarChart.plot_points(voltages)
+	log_to_console("Voltages are as follows: " + outputs[1])
 	$MultiplotChart.plot_points(voltages)
 
 
 func _on_button_2_pressed() -> void:
 	$MultiplotChart.reset_chart()
 	#print(Python.exec_test_mircatsdk_module("hey there", 11))
+
+func log_to_console(text):
+	$Console.text = text
